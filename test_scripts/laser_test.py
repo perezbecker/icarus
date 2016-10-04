4d821df59165eef684c7ee9daed324977630dffa
picarus <pi@icarus.com>
2016-10-03 19:48:23 -0700

    modified lidar_test, added laser_test

---
+++ test_scripts/laser_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+
+# turn on light for 20 seconds when button pushed
+
+# we will use the pin numbering of the SoC, so our pin numbers in the code are
+# the same as the pin numbers on the gpio headers
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
+
+GPIO.output(12, False)
+
+try:
+  GPIO.output(12, True)
+  time.sleep(20)
+  GPIO.output(12, False)
+
+except KeyboardInterrupt:
+  GPIO.output(18, False)
